fbca16a51182a639e82f208fd0949af02b6cd5af
Boris Draaisma <boris.draaisma@hotmail.com>
2020-11-24 12:11:26 +0100

    Created ConnectPage And ConnectViewModel.

---
+++ MobileApp/MobileApp/ViewModels/ConnectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MobileApp.ViewModels
+{
+    public class ConnectViewModel : BaseViewModel
+    {
+        public ConnectViewModel()
+        {
+            Title = "ConnectPage";
+            
+        }
+    }
+}
