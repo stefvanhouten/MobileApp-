ab711a367d97b3b0af80bd516c9029c7eb49b4ee
stefvanhouten <stefvanhouten@gmail.com>
2020-12-10 13:53:50 +0100

    Attached connectpage to MQTT. Now uses input to try and connect.

--- MobileApp/MobileApp/ViewModels/ConnectViewModel.cs
+++ MobileApp/MobileApp/ViewModels/ConnectViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileApp.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,26 +12,24 @@ namespace MobileApp.ViewModels
     {
         
         public Command ConnectClickCommand { get; set; }
-        public string PortInput { get; set; }
-        public string IPInput { get; set; }
+        public int PortInput { get; set; } = 1883; //Default port
+        public string IPInput { get; set; } = "127.0.0.1"; //Default localhost
 
       
         public ConnectViewModel()
         {
             Title = "ConnectPage";
-            PortInput = "1883";
-            IPInput = "";
             ConnectClickCommand = new Command(ConnectClick);
-            
-           
         }
 
        
 
         public void ConnectClick()
         {
+            App.ServerIP = this.IPInput;
+            App.ServerPort = this.PortInput;
+            Application.Current.MainPage.Navigation.PushAsync(new DashboardPage(), false);
             Console.WriteLine(PortInput);
-
         }
        
         
