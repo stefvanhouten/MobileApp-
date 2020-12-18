74daab5579fe6d3deac2dd717b4272802aeaebdc
SDanDyS <gameking23@hotmail.com>
2020-12-18 03:00:14 -0800

    saving page to merge

---
+++ MobileApp/MobileApp/Models/MoistMeter.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+
+namespace MobileApp.Models
+{
+    public class MoistMeter
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ MobileApp/MobileApp/ViewModels/GroundMoistureViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileApp.ViewModels
+{
+    class GroundMoistureViewModel : BaseViewModel
+    {
+        public GroundMoistureViewModel(string topic)
+        {
+            Title = "Ground Moisture";
+            App.Client.GetAllMessagesFromTopic(topic);
+        }
+    }
+}
