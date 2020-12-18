29a12b74dda491cb407fdb6ed7976be00c99aaf1
wavez101 <71439143+wavez101@users.noreply.github.com>
2020-12-18 11:59:12 +0100

    Created ProductViewModel and ProductPage. Also Created Button to GroundMoisturePage.

---
+++ MobileApp/MobileApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MobileApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp.ViewModels
+{
+    class ProductViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        public Command<string>GroundMoistButtonClickCommand { get; set; }
+
+
+        public ProductViewModel()
+        {
+            GroundMoistButtonClickCommand = new Command<string>(GroundMoistButtonClick);
+        }
+
+
+        public void GroundMoistButtonClick(string WateringSystemFeedback)
+        {
+
+            Application.Current.MainPage.Navigation.PushAsync(new GroundMoisturePage(WateringSystemFeedback), true);
+        }
+
+    }
+}
