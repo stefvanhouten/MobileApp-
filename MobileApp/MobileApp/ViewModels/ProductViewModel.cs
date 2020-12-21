a590e0265c93b13811f2ca5e868a56dc545c2841
wavez101 <71439143+wavez101@users.noreply.github.com>
2020-12-21 14:16:34 +0100

    Created Coffee On/Off Button. (Needs to be checked)

--- MobileApp/MobileApp/ViewModels/ProductViewModel.cs
+++ MobileApp/MobileApp/ViewModels/ProductViewModel.cs
@@ -12,20 +12,65 @@ namespace MobileApp.ViewModels
 {
     class ProductViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public Command<string>GroundMoistButtonClickCommand { get; set; }
+        private string _CurrentCoffeeStatus = "Coffee";
 
+        public bool CoffeeStatusIsOff;
+        public Command<string>GroundMoistButtonClickCommand { get; set; }
+        public Command<string>CoffeeSwitchClickCommand { get; set; }
+
+        public string CurrentCoffeStatus {
+            get { return _CurrentCoffeeStatus; }
+            set
+            {
+               _CurrentCoffeeStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void OnOfSwitch()
+        {
+            if (CoffeeStatusIsOff)
+            {
+                CurrentCoffeStatus = "Coffee Off";
+                CoffeeStatusIsOff = false;
+            }
+            else
+            {
+                CurrentCoffeStatus = "Coffee On";
+                CoffeeStatusIsOff = true;
+            }
+        }
 
         public ProductViewModel()
         {
             GroundMoistButtonClickCommand = new Command<string>(GroundMoistButtonClick);
+            CoffeeSwitchClickCommand = new Command<string>(CoffeeSwitchClick);
         }
 
 
         public void GroundMoistButtonClick(string WateringSystemFeedback)
         {
 
-            Application.Current.MainPage.Navigation.PushAsync(new GroundMoisturePage(WateringSystemFeedback), true);
+            Application.Current.MainPage.Navigation.PushAsync(new GroundMostuirePage(WateringSystemFeedback), true);
         }
 
+        public void CoffeeSwitchClick(string CoffeeOnOffFeedback)
+        {
+            OnOfSwitch();
+
+            if (CoffeeStatusIsOff)
+            {
+                App.Client.Publish("Coffee", "ON");
+                
+            }
+            else
+            {
+                App.Client.Publish("Coffee", "OFF");
+                
+            }
+          
+
+
+        }
     }
 }
