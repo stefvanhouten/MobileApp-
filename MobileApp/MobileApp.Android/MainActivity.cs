a10948b0ee579d7b757aabca09db515a8222d6e3
Stef van Houten <stefvanhouten@gmail.com>
2021-01-04 14:12:38 +0100

    Finished splash screen

--- MobileApp/MobileApp.Android/MainActivity.cs
+++ MobileApp/MobileApp.Android/MainActivity.cs
@@ -9,14 +9,13 @@ using Android.OS;
 
 namespace MobileApp.Droid
 {
-    [Activity(Label = "MobileApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MobileApp")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
